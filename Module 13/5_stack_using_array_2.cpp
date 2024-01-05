0a7efbeedc707bfbd57acdab3549f7e65958f6a9
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-01-06 01:16:00 +0600

    Module 13

---
+++ Module 13/5_stack_using_array_2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class myStack
+{
+    public:
+        vector<int>v;
+        void push(int val)
+        {
+            v.push_back(val);
+        }
+        void pop()
+        {
+            v.pop_back();
+        }
+        int top()
+        {
+            return v.back();
+        }
+        int size()
+        {
+            return v.size();
+        }
+        bool empty()
+        {
+            if (v.size()==0) return true;
+            else return false;
+        }
+};
+int main()
+{
+    myStack st;
+    int n; cin>>n;
+    for (int i=0; i<n; i++)
+    {
+        int v; cin>>v;
+        st.push(v);
+    }
+    while (!st.empty())
+    {
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+    return 0;
+}
